a68d9bf54f86c17b9605c359af020c2ac9b61bc9
georgeslight <georgeslight@gmail.com>
2025-11-16 22:11:50 +0100

    gtk

--- app/gtk.nix
+++ app/gtk.nix
@@ -1,11 +1,27 @@
 { config, pkgs, ... }:
 
 {
+
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      # gtk-theme = "Tokyonight-Dark";
+      gtk-theme = "adw-gtk3-dark";
+    };
+  };
+
   gtk = {
     enable = true;
+    font = {
+      # package = pkgs.nerd-fonts.jetbrains-mono;
+      name = "Noto Sans";
+      size = 10;
+    };
     theme = {
-      package = pkgs.tokyonight-gtk-theme;
-      name = "Tokyonight-Dark";
+      # package = pkgs.tokyonight-gtk-theme;
+      # name = "Tokyonight-Dark";
+      package = pkgs.adw-gtk3;
+      name = "adw-gtk3-dark";
     };
     cursorTheme = {
       package = pkgs.catppuccin-cursors;
@@ -16,5 +32,33 @@
       package = pkgs.tokyonight-gtk-theme;
       name = "Tokyonight-Dark";
     };
+
+    gtk4.extraConfig = {
+      gtk-application-prefer-dark-theme = true;
+      gtk-cursor-blink = true;
+      gtk-cursor-blink-time = 1000;
+      gtk-decoration-layout = ":minimize,maximize,close";
+      gtk-enable-animations = true;
+      gtk-primary-button-warps-slider = true;
+      gtk-sound-theme-name = "freedesktop";
+      gtk-xft-dpi = 98304;
+    };
+    gtk3.extraConfig = {
+      gtk-application-prefer-dark-theme = true;
+      gtk-button-images = true;
+      gtk-cursor-blink = true;
+      gtk-cursor-blink-time = 1000;
+      gtk-decoration-layout = ":minimize,maximize,close";
+      gtk-enable-animations = true;
+      gtk-primary-button-warps-slider = true;
+      gtk-sound-theme-name = "freedesktop";
+      gtk-xft-dpi = 98304;
+      gtk-menu-images = true;
+      gtk-modules = "colorreload-gtk-module:window-decorations-gtk-module";
+      gtk-toolbar-style = 3;
+    };
+
   };
+  # home.sessionVariables.GTK_THEME = "Tokyonight-Dark";
+  home.sessionVariables.GTK_THEME = "adw-gtk3-dark";
 }
